545c5529882137f80a82c3746f3e9136e80a9aa1
Mugiira John Kevin Murega <jmugiira@hq.ekenya.co.ke>
2025-01-14 12:19:48 +0300

    updated the gateway to use keycloak security

--- api-gateway/src/main/java/com/netco/microservices/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/netco/microservices/apigateway/config/SecurityConfig.java
@@ -18,7 +18,15 @@ package com.netco.microservices.apigateway.config;
 //import java.util.List;
 
 
-//@Configuration
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 //@EnableWebSecurity
 public class SecurityConfig {
 
@@ -38,25 +46,21 @@ public class SecurityConfig {
 //                .build();
 //    }
 //
-//    @Bean
-//    public JwtDecoder jwtDecoder() {
-//        return JwtDecoders.fromIssuerLocation("http://localhost:8181/realms/netco-realm");
-//    }
-////    @Bean
-////    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-////        http
-////                .csrf(AbstractHttpConfigurer::disable)
-////                .authorizeHttpRequests(auth -> auth
-////                        .requestMatchers("/eureka/**").permitAll()
-////                        .anyRequest().authenticated()
-////                )
-////                .oauth2ResourceServer(oauth2 -> oauth2
-////                        .jwt(Customizer.withDefaults())
-////                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-////                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
-////                );
-////        return http.build();
-////    }
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation("http://localhost:8181/realms/netco-realm");
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+//                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/eureka/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()))
+                .build();
+    }
 //
 //    @Bean
 //    CorsConfigurationSource corsConfigurationSource() {
